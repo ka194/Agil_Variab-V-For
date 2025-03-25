68daf4e923b2de10bd4b967086a615e3862146fe
Ka194 <luzkarinareyes5@gmail.com>
2025-03-24 22:57:09 -0600

    Subiendo archivos iniciales

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Alumnos from './components/Alumnos.vue';
+</script>
+
+<template>
+
+
+<Alumnos/>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const bienvenida = ref("Bienvenidos a la seccion de condicionales")
+const nombre = ref("Luz Karina")
+const apellidos = ref("Reyes López")
+
+
+
+</script>
+
+<template>
+ <div class="container">
+    <h2 class="text-center">{{bienvenida}}</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <h6 class="">{{ nombre }}</h6>
+        <h6>{{apellidos  }}</h6>
+      </div>
+    </div>
+  </div> 
+
+
+</template>
+
+<style scoped> </style>
