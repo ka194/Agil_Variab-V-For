2749f0feb6c0415c8f491dea5e1c5dd42b07b73d
Ka194 <luzkarinareyes5@gmail.com>
2025-03-25 19:41:55 -0600

    Horario.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Recorrido from './components/Recorrido.vue';
 import Alumnos from './components/Alumnos.vue';
+import Horario from './components/Horario.vue';
 </script>
 
 <template>
-
-
-<Alumnos/>
-</template>
+    <!-- <Recorrido /> -->
+    <!-- <Alumnos /> -->
+    <Horario />
+  </template>
 
 <style scoped></style>
